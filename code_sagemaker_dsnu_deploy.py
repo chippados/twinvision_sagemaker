05e31a501d257e7549ac5f0e8363bfac114fa92d
Fermaster890-sudo <ferperezine@gmail.com>
2025-10-05 21:01:06 -0300

    Update code_sagemaker_dsnu_deploy.py

--- code_sagemaker_dsnu_deploy.py
+++ code_sagemaker_dsnu_deploy.py
@@ -6,7 +6,7 @@ sagemaker_session = sagemaker.Session()
 role = sagemaker.get_execution_role()
 
 tf_model = TensorFlowModel(
-                        model_data               = 's3://sagemaker-us-east-1-435133245123/tensorflow-training-2025-10-05-22-41-18-493/source/sourcedir.tar.gz',
+                        model_data               = 's3://sagemaker-us-east-1-435133245123/tensorflow-training-2025-10-05-23-35-14-668/model/tensorflow_model/1/model.tar.gz',
                         role                     = role,
                         framework_version        ='2.11.0',
                         entry_point              = 'inference_script_dsnu.py',
